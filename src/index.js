ad9b5a5125911c82ee4078d51fead82abfbd3b2c
Wilhelm Uggla <willeuggla@gmail.com>
2022-05-31 22:03:19 +0200

    restructure

--- src/index.js
+++ src/index.js
@@ -3,12 +3,47 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+  useAccount
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+import {
+  getDefaultWallets,
+  lightTheme,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+
+const { chains, provider } = configureChains(
+  [chain.polygonMumbai],
+  [
+    alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <WagmiConfig client={wagmiClient}>
+      <App chains={chains} provider={provider} />
+    </WagmiConfig>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
